971502d77a4322bfe58e7d3c78b7e56e1604ccd7
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-06-07 03:31:34 +0100

    Update sing-Up.php

--- today6]6/sing-Up.php
+++ today6]6/sing-Up.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    include "all.php";
+    include "all2.php";
     if (!empty($_POST['username'])) {
         $username = filter($_POST['username']);
         
@@ -8,11 +8,21 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && strlen($_POST["email"]) <= 50) {
 
                 $email = filter($_POST['email']);
+                // countryCode
+                if (!empty($_POST["countryCode"])) {
+                    $typeNumber = $_POST["countryCode"];
+
                 if (!empty($_POST["phone"])) {
-                     $phone = filter_phone($_POST["phone"]);
-                     
-                  if (filter_var($phone, FILTER_VALIDATE_INT) && strlen($_POST["phone"]) >= 13 && preg_match('/^\d{10,15}$/', $phone) ) {
-                        $phone = '+'.filter_phone($_POST['phone']);
+                     $phone = filter($_POST["phone"]);
+                     $lengthphone ="";
+                     $valuesphone ="";
+                for($i=0;$i<strlen( $phone) ;$i++){
+                    $lengthphone =$i;
+                    $valuesphone = $phone[$i] ; 
+
+                }
+                     if (filter_var($valuesphone, FILTER_VALIDATE_INT) && $lengthphone >= 9 ) {
+                       
                         $age='';
                 if (!empty($_POST['password'])) {
   
@@ -36,36 +46,27 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                                     // echo "emailaready";
                                 } else {
 
+                                    // Full texts
+                                    // client_id	
+                                    // client_fullname	
+                                    // client_email	
+                                    // client_ty
                                                                                                 
                                     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                                    $insert = $connect->prepare("INSERT INTO $tbname(client_fullname,client_email,client_phone,client_age,client_password) VALUE(:username, :email, :phone, :age,:password)");
+                                    $insert = $connect->prepare("INSERT INTO $tbname(client_fullname,client_email,client_ty,client_phone,client_age,client_password) VALUE(:username, :email,:typeNumber ,:phone, :age,:password)");
 
                                     $insert->bindParam(":username", $username);
                                     $insert->bindParam(":email", $email);
                                     $insert->bindParam(":password", $password);
                                     $insert->bindParam(":phone", $phone);
+                                    $insert->bindParam(":typeNumber", $typeNumber);
                                     $insert->bindParam(":age",$age);
 
                                     $insert->execute();
 
                                     $verified = "verified";
                                     echo $verified;
-                                //signupdb
-                                    
-                                    $selectsingupdb =  $connect->query("SELECT singUp FROM $tbnamedashbord ");
-                                    $signupnumber = $selectsingupdb->fetch();
-                                    $number = $signupnumber['singUp'];
-
-                                     //add
-                                     if($verified == "verified"){  
-                                     $number++;
-                                     //update                                  
-                                     $updatesingup = "UPDATE $tbnamedashbord SET singUp='$number'";
-                                     $stmt = $connect->prepare($updatesingup);
-                                     $stmt->execute();
-                                              }
-
-                                //signupdb
+                              
                            
                                 }
                             } catch (PDOException $e) {
@@ -89,6 +90,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             echo "phoneempty";
         }
         // 
+    } else {
+        echo "countryCodeempty";
+    }
             } else {
                 echo "emailbad";
             }
@@ -102,35 +106,35 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
  
 
 <?php
-if(isset($_POST["googleSign"])){
-require_once 'vendor/autoload.php';
+// if(isset($_POST["googleSign"])){
+// require_once 'vendor/autoload.php';
 
-$clientID = '27634668984-5ooshfi469quhah2nmbaj6jpcdot12hd.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectUri = 'http://localhost/SAMIR/BOKINGG/BOKINGG8/google/SOSIAL.LOGIN.html';
+// $clientID = '27634668984-5ooshfi469quhah2nmbaj6jpcdot12hd.apps.googleusercontent.com';
+// $clientSecret = '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';
+// $redirectUri = 'http://localhost/SAMIR/BOKINGG/BOKINGG8/google/SOSIAL.LOGIN.html';
 
-$client = new Google_Client();
-$client->setClientId($clientID);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri($redirectUri);
-$client->addScope("email");
-$client->addScope("profile");
+// $client = new Google_Client();
+// $client->setClientId($clientID);
+// $client->setClientSecret($clientSecret);
+// $client->setRedirectUri($redirectUri);
+// $client->addScope("email");
+// $client->addScope("profile");
 
-if (isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token['access_token']);
+// if (isset($_GET['code'])) {
+//     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+//     $client->setAccessToken($token['access_token']);
 
-    $google_oauth = new Google_Service_Oauth2($client);
-    $google_account_info = $google_oauth->userinfo->get();
-    $email = $google_account_info->email;
-    $name = $google_account_info->name;
+//     $google_oauth = new Google_Service_Oauth2($client);
+//     $google_account_info = $google_oauth->userinfo->get();
+//     $email = $google_account_info->email;
+//     $name = $google_account_info->name;
 
-    // Use the obtained user info here
+//     // Use the obtained user info here
 
-} else {
-    $authUrl = $client->createAuthUrl();
-    echo json_encode(['link' => $authUrl]);
-    echo $authUrl;
-}
-}
+// } else {
+//     $authUrl = $client->createAuthUrl();
+//     echo json_encode(['link' => $authUrl]);
+//     echo $authUrl;
+// }
+// }
 ?>
